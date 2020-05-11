d6d09cef71e2031211e6c5a0f74c9e2c91400b2e
Vincent Parizet <vincent.parizet@hotmail.fr>
2020-05-11 17:20:03 +0200

    add part1 tests

--- tests/part1_tb.vhd
+++ tests/part1_tb.vhd
@@ -149,6 +149,109 @@ begin
         -- tests
         wait for 10 ns;
 
+        -- L’addition d’1 registre avec une valeur immédiate
+        Imm <= "00000010";
+        RegWE <= '0';
+        MemWE <= '0';
+        RA <= "0001";
+        RB <= (others => '0');
+        RW <= (others => '0');
+        AluInBCom <= '1'; -- select imm
+        RegWCom <= '0';
+        OP <= "00"; -- addition
+        wait for 10 ns;
+
+        Ok <= AluOut = X"00000002";
+
+        -- ecrit AluOut dans Registre 0
+        RegWE <= '1';
+        wait for 10 ns;
+        -- reg0  = X"00000002"
+        -- reg15 = X"00000030"
+
+        -- L’addition de 2 registres
+        RegWE <= '0';
+        MemWE <= '0';
+        RA <= "0000";
+        RB <= "1111";
+        RW <= (others => '0');
+        AluInBCom <= '0'; -- select 2nd reg
+        RegWCom <= '0';
+        OP <= "00"; -- addition
+        wait for 10 ns;
+
+        Ok <= AluOut = X"00000032";
+
+        -- La soustraction de 2 registres
+        RegWE <= '0';
+        MemWE <= '0';
+        RA <= "1111";
+        RB <= "0000";
+        RW <= (others => '0');
+        AluInBCom <= '0'; -- select 2nd reg
+        RegWCom <= '0';
+        OP <= "10"; -- soustraction
+        wait for 10 ns;
+
+        Ok <= AluOut = X"0000002E";
+
+        -- La soustraction d’1 valeur immédiate à 1 registre
+        Imm <= "00000001";
+        RegWE <= '0';
+        MemWE <= '0';
+        RA <= "1111";
+        RB <= (others => '0');
+        RW <= (others => '0');
+        AluInBCom <= '1'; -- select imm
+        RegWCom <= '0';
+        OP <= "10"; -- addition
+        wait for 10 ns;
+
+        Ok <= AluOut = X"0000002F";
+
+        -- La copie de la valeur d’un registre dans un autre registre
+        RegWE <= '1';
+        MemWE <= '0';
+        RA <= "1111";
+        RB <= "0010";
+        RW <= "0010";
+        AluInBCom <= '0'; -- select 2nd reg
+        RegWCom <= '0';
+        OP <= "11"; -- keep A
+        wait for 10 ns;
+
+        RegWE <= '0';
+        Ok <= A = B;
+        wait for 10 ns;
+
+        -- L’écriture d’un registre dans un mot de la mémoire.
+        Imm <= "00001111";
+        RegWE <= '0';
+        MemWE <= '1';
+        RA <= "0000";
+        RB <= "1111";
+        RW <= "0010";
+        AluInBCom <= '1'; -- select 2nd reg
+        RegWCom <= '0';
+        OP <= "01"; -- keep B
+        wait for 10 ns;
+
+        -- La lecture d’un mot de la mémoire dans un registre.
+        Imm <= "00001111";
+        RegWE <= '1';
+        MemWE <= '0';
+        RA <= "0111";
+        RB <= "1111";
+        RW <= "0111";
+        AluInBCom <= '1'; -- select 2nd reg
+        RegWCom <= '1'; -- W = DataOut
+        OP <= "01"; -- keep B
+        wait for 10 ns;
+
+        Ok <= A = B;
+        wait for 10 ns;
+        Ok <= A = X"00000030";
+
         wait;
     end process;
 end architecture;
